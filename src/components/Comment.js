eb0d3091ccbbb26b4560f6d8f0bc98ad4432b9b4
Lu Melamed <lumelamed@gmail.com>
2024-11-26 12:36:57 -0300

    primera prueba de Context

---
+++ src/components/Comment.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useContext } from "react";
+import UserInfoContext from "../context/UserInfoContext";
+
+export default function Comment() {
+  const { username, isAdmin } = useContext(UserInfoContext);
+  return (
+    <div>
+      <h3>Comment</h3>
+      <p>Logged in as {username}</p>
+      {isAdmin && <button>Edit comment</button>}
+    </div>
+  );
+}
